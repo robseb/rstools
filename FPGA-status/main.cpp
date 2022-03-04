28851d63cd1be7bdce3d3b4287443873c4450db0
robseb <git@robseb.de>
2022-03-04 10:52:25 +0000

    [+] FPGA-status done (1.00)

--- FPGA-status/main.cpp
+++ FPGA-status/main.cpp
@@ -51,6 +51,8 @@ using namespace std;
 #define REG_SYSMAN_GBL_OFFSET		0x20
 #define REG_SYSMAN_INDIV			0xFFD08024
 #define REG_SYSMAN_INDIV_OFFSET		0x24
+#define REG_SYSMAN_MODULE			0xFFD08028
+#define REG_SYSMAN_MODULE_OFFSET	0x28
 
 
 #define REG_WDT0_BASE				0xFFD02000
@@ -59,6 +61,9 @@ using namespace std;
 #define REG_WDT1_BASE				0xFFD03000
 #define REG_WDT1_OFFFSET			0x00
 
+
+#define REG_CLCK_CTRL				0xFFD04000
+#define REG_CLCK_CTRL_OFFFSET		0x00
 /*
 * Global Values
 */
@@ -75,6 +80,9 @@ volatile uint32_t* ptrwdt0;
 
 volatile void* wdt1Map;    			
 volatile uint32_t* ptrwdt1;
+
+volatile void* clkmgrMap;    			
+volatile uint32_t* ptrclkmgr;
 
 /*
 *   @brief               Convert MSEL Code to String with mode description 
@@ -197,46 +205,105 @@ string indiv2str(uint8_t indiv_code)
 {	
 	string msg="";
 	if(indiv_code & (1<<0)) 
-		msg = "		L 	Reset request interface is enabled. Logic in the FPGA fabric can reset the HPS.\n";
+		msg = "		L 	[Y] Reset request interface is enabled. Logic in the FPGA fabric can reset the HPS.\n";
 	else
-		msg = "		L 	Reset request interface is disabled. Logic in the FPGA fabric cannot reset the HPS.\n"; 	
+		msg = "		L 	[N] Reset request interface is disabled. Logic in the FPGA fabric cannot reset the HPS.\n"; 	
 	
 	if(indiv_code & (1<<1)) 
-		msg =msg+ "		L 	Enables the fpgajtagen bit found in the ctrl register.\n";
+		msg =msg+ "		L 	[Y] Enables the fpgajtagen bit found in the ctrl register.\n";
 	else
-		msg =msg+ "		L	Disables the fpgajtagen bit found in the ctrl register.\n"; 	
+		msg =msg+ "		L	[N] Disables the fpgajtagen bit found in the ctrl register.\n"; 	
 	
 	if(indiv_code & (1<<2)) 
-		msg =msg+ "		L 	CONFIG_IO interface is enabled. Execution of the CONFIG_IO instruction \n"+\
+		msg =msg+ "		L 	[Y] CONFIG_IO interface is enabled. Execution of the CONFIG_IO instruction \n"+\
 				  "            		in the FPGA JTAG TAP controller is supported.\n";
 	else
-		msg =msg+ "		L	CONFIG_IO interface is disabled. Execution of the CONFIG_IO instruction in the FPGA JTAG TAP controller\n"+ \
+		msg =msg+ "		L	[N] CONFIG_IO interface is disabled. Execution of the CONFIG_IO instruction in the FPGA JTAG TAP controller\n"+ \
 				  "           		is unsupported and produces undefined results.\n"; 	
 
 	if(indiv_code & (1<<3)) 
-		msg =msg+ "		L 	Boundary-scan interface is enabled. Execution of the boundary-scan instructions \n"+\
+		msg =msg+ "		L 	[Y] Boundary-scan interface is enabled. Execution of the boundary-scan instructions \n"+\
 				  "            		in the FPGA JTAG TAP controller is supported.\n";
 	else
-		msg =msg+ "		L 	Boundary-scan interface is disabled. Execution of boundary-scan instructions in the FPGA JTAG TAP\n"+ \
+		msg =msg+ "		L 	[N] Boundary-scan interface is disabled. Execution of boundary-scan instructions in the FPGA JTAG TAP\n"+ \
 				  "            		controller is unsupported and produces undefined results.\n"; 	
 
 	if(indiv_code & (1<<4)) 
-		msg =msg+ "		L	Trace interface is enabled. Other registers in the HPS debug logic must be programmmed to\n" +\
+		msg =msg+ "		L	[Y] Trace interface is enabled. Other registers in the HPS debug logic must be programmmed to\n" +\
 				  " 		 actually send trace data to the FPGA fabric.\n";
 	else
-		msg =msg+ "		L 	Trace interface is disabled. HPS debug logic cannot send trace data to the FPGA fabric.\n";
+		msg =msg+ "		L 	[N] Trace interface is disabled. HPS debug logic cannot send trace data to the FPGA fabric.\n";
 
 	if(indiv_code & (1<<6)) 
-		msg =msg+ "		L 	STM event interface is enabled. Logic in the FPGA fabric can trigger STM events.\n";
+		msg =msg+ "		L 	[Y] STM event interface is enabled. Logic in the FPGA fabric can trigger STM events.\n";
 	else
-		msg =msg+ "		L	STM event interface is disabled. Logic in the FPGA fabric cannot trigger STM events.\n";
+		msg =msg+ "		L	[N] STM event interface is disabled. Logic in the FPGA fabric cannot trigger STM events.\n";
 	
 	if(indiv_code & (1<<7)) 
-		msg =msg+ "		L 	FPGA Fabric can send triggers.\n";
+		msg =msg+ "		L 	[Y] FPGA Fabric can send triggers.\n";
 	else
-		msg =msg+ "		L 	FPGA Fabric cannot send triggers.\n";
+		msg =msg+ "		L 	[N] FPGA Fabric cannot send triggers.\n";
 
 	return msg;				
+}
+
+/*
+*   @brief               Convert System Manger Module Register to description string
+*   @param	indiv_code	 register (acording datasheet)
+*   @return              description string
+*/ 
+string moudleEn2str(uint8_t regitser)
+{	
+	string msg="";
+	if(regitser & (1<<0)) 
+		msg = "		L 	[Y] Enable signals from FPGA fabric to HPS SPIM0 module interface.\n";
+	else
+		msg = "		L 	[N] Disable signals from FPGA fabric to HPS SPIM0 module interface.\n"; 
+	
+	if(regitser & (1<<1)) 
+		msg =msg+= "		L 	[Y] Enable signals from FPGA fabric to HPS SPIM1 module interface.\n";
+	else
+		msg =msg+= "		L 	[N] Disable signals from FPGA fabric to HPS SPIM1 module interface.\n"; 	
+	
+	if(regitser & (1<<2)) 
+		msg =msg+= "		L 	[Y] Enable signals from FPGA fabric to HPS EMAC0 module interface.\n";
+	else
+		msg =msg+= "		L 	[N] Disable signals from FPGA fabric to HPS EMAC0 module interface.\n"; 
+
+	if(regitser & (1<<3)) 
+		msg =msg+= "		L 	[Y] Enable signals from FPGA fabric to HPS EMAC1 module interface.\n";
+	else
+		msg =msg+= "		L 	[N] Disable signals from FPGA fabric to HPS EMAC1 module interface.\n"; 
+
+	if(regitser & (1<<5)) 
+		msg =msg+= "		L 	[Y] Enable signals from FPGA fabric to HPS SD/MMC controller module interface.\n";
+	else
+		msg =msg+= "		L 	[N] Disable signals from FPGA fabric to HPS SD/MMC controller module interface.\n"; 	
+	
+
+	return msg;	
+}
+
+
+/*
+*   @brief               Convert Clock Controll Register to description string
+*   @param	indiv_code	 register (acording datasheet)
+*   @return              description string
+*/ 
+string clockCtrl2str(uint8_t regitser)
+{	
+	string msg="";
+	if(regitser & (1<<0)) 
+		msg = "		L 	[Y] Safe Mode Enabled!: Main PLL hardware-managed clocks are bypassed and osc1_clk is used\n";
+	else
+		msg = "		L 	[N] Safe Mode Disabled: Hardware-managed clock are in use\n"; 
+	
+	if(regitser & (1<<2)) 
+		msg =msg+= "		L 	[Y] After the warm reset, Safe Mode will be activated automatically\n";
+	else
+		msg =msg+= "		L 	[N] After the warm reset, Safe Mode will not enabled!\n"; 
+
+	return msg;	
 }
 
 /*
@@ -285,7 +352,7 @@ int initMemRegs(void)
 	//
 
     // Create a Memory Map to acess the System Manger
-	systemMangerMap = mmap(NULL,REG_SYSMAN_INDIV_OFFSET+4, PROT_READ, MAP_PRIVATE, posix_fd, \
+	systemMangerMap = mmap(NULL,REG_SYSMAN_MODULE_OFFSET+4, PROT_READ, MAP_PRIVATE, posix_fd, \
 		REG_SYSMAN_BASE);
 
     if (systemMangerMap == MAP_FAILED)
@@ -349,6 +416,31 @@ int initMemRegs(void)
         return -1;
     }
 
+
+	//
+	// Clock Manager Address Space
+	//
+
+    // Create a Memory Map
+	clkmgrMap = mmap(NULL,4, PROT_READ, MAP_PRIVATE, posix_fd, \
+		REG_CLCK_CTRL);
+
+    if (clkmgrMap == MAP_FAILED)
+	{
+		cout << "\n[ERROR] Failed to open the memory maped interface to the Clock Manager" << endl;
+        return -1;
+	}
+
+    // Allocate a pointer to the maped address space 
+    ptrclkmgr = (uint32_t*)clkmgrMap;
+
+    if (ptrclkmgr==0) 
+    {
+        cout << "\n[ERROR] Failed to allocate a ponter to the to the Clock Manager"<<endl;
+        return -1;
+    }
+
+
     return 1;
 }
 
@@ -364,9 +456,19 @@ void deinit(void)
         munmap((void*) fpgaMangerMap, 4);
     }
 
-	if (systemMangerMap != MAP_FAILED)
+	if (wdt0Map != MAP_FAILED)
 	{
-		munmap((void*) systemMangerMap, REG_SYSMAN_INDIV_OFFSET+4);
+		munmap((void*) wdt0Map, REG_WDT0_BASE);
+	}
+
+	if (wdt1Map != MAP_FAILED)
+	{
+		munmap((void*) wdt1Map, REG_WDT1_BASE);
+	}
+
+	if (clkmgrMap != MAP_FAILED)
+	{
+		munmap((void*) clkmgrMap, REG_CLCK_CTRL);
 	}
 
     // close POSIX port
@@ -464,7 +566,7 @@ uint8_t readSysManIndiv(void)
 */ 
 uint16_t readSiliconRev(void)
 {	
-	if (ptrsystemManger==0) return 0;
+	if (ptrsystemManger==0) return 255;
 	uint32_t rev = *(ptrsystemManger + (REG_SYSMAN_SILID_OFFSET/4)) & 0xFFFF;
 	return (uint16_t) rev; 
 }
@@ -475,10 +577,32 @@ uint16_t readSiliconRev(void)
 */ 
 uint16_t readSiliconID(void)
 {	
-	if (ptrsystemManger==0) return 0;
+	if (ptrsystemManger==0) return 255;
 	uint32_t id = *(ptrsystemManger + (REG_SYSMAN_SILID_OFFSET/4)) & 0xFFFF0000;
 	return (uint16_t) (id >> 16); 
 }
+
+/*
+*   @brief              Read module Signal Enable Register
+*   @return             Register [5:0]  
+*/ 
+uint8_t readModuleSignalEn(void)
+{	
+	if (ptrsystemManger==0) return 255;
+	uint32_t module = *(ptrsystemManger + (REG_SYSMAN_MODULE_OFFSET/4)) & 0x1F;;
+	return (uint8_t) module; 
+}
+
+/*
+*   @brief              Read the Clock Manger Controll Register
+*   @return             indiv register [7:0]
+*/ 
+uint8_t readClockCtrl(void)
+{	
+	if (ptrclkmgr==0) return 255;
+	return *(ptrclkmgr + (REG_CLCK_CTRL_OFFFSET/4)) & 0x7;
+}
+
 
 /*
 *   @brief              Check if WatchDog 0 is enbaled
@@ -518,18 +642,20 @@ int main(int argc, const char* argv[])
 	uint8_t  watchDog1_en   = WatchDog1Enabled();
 	uint8_t  global_inf_en  = isGlobalInterfaceEnbaled();
 	uint8_t  indiv_code		= readSysManIndiv();
+	uint8_t  signal_en		= readModuleSignalEn();
+	uint8_t  clock_ctrl		= readClockCtrl();
 	
 	string stat ="";
 
 	if ((argc > 1) && (std::string(argv[1]) == "-h"))
 	{
-		cout << "	Command to read current Status mode of the FPGA fabric" << endl;
+		cout << "	Command to read current Status of the HPS and FPGA Fabric" << endl;
 		cout << "	FPGA-status" << endl;
-		cout << "		read the status with detailed output" << endl;
+		cout << "		Read the status with detailed output" << endl;
+		cout <<endl <<"Vers.: "<<VERSION<<endl;
+		cout <<"Copyright (C) 2021-2022 rsyocto GmbH & Co. KG" << endl;
+
 	}
-	else if ((argc > 1) && (std::string(argv[1]) == "-d"))
-		// Print only the Status Code 
-		cout << stat;
 	else
 	{
 		// Print the MSEL Value as detailed string 
@@ -545,11 +671,11 @@ int main(int argc, const char* argv[])
 		cout << bsl2str(bsel_code) << endl;
 
 		cout << "# HPS Info:" <<endl;
-		if (is_dualcore==1) cout << "	        Is dual-core (CPU0 and CPU1 both available)."<<endl;
-		else				cout << "	        Not dual-core (only CPU0 available)."<<endl;
+		if (is_dualcore==1) cout << "	        [Y] Is dual-core (CPU0 and CPU1 both available)."<<endl;
+		else				cout << "	        [N] Not dual-core (only CPU0 available)."<<endl;
 		
-		if (has_can==1) 	cout << "	        CAN0 and CAN1 are available"<<endl;
-		else				cout << "	        CAN0 and CAN1 are not available"<<endl;
+		if (has_can==1) 	cout << "	        [Y] CAN0 and CAN1 are available"<<endl;
+		else				cout << "	        [N] CAN0 and CAN1 are not available"<<endl;
 		
 		cout << "	        Silicon revision No: "<<silicon_rev;
 		if 		(silicon_rev==0x1) cout << " (First Silicon)"<<endl;
@@ -561,18 +687,24 @@ int main(int argc, const char* argv[])
 		
 		cout << "# WatchDog Status:" <<endl;
 
-		if (watchDog0_en==1) 	cout << "	  L     Watchdog 0 enabled and generates a warm reset request"<<endl;
-		else					cout << "	  L     Watchdog 0 disabled"<<endl;
+		if (watchDog0_en==1) 	cout << "	  L     [Y] Watchdog 0 enabled and generates a warm reset request"<<endl;
+		else					cout << "	  L     [N] Watchdog 0 disabled"<<endl;
 
-		if (watchDog1_en==1) 	cout << "	  L     Watchdog 1 enabled and generates a warm reset request"<<endl;
-		else					cout << "	  L     Watchdog 1 disabled"<<endl;
+		if (watchDog1_en==1) 	cout << "	  L     [Y] Watchdog 1 enabled and generates a warm reset request"<<endl;
+		else					cout << "	  L     [N] Watchdog 1 disabled"<<endl;
 
 		cout << "#  Interfaces/Signals between the FPGA and HPS:" <<endl;
 
-		if (global_inf_en==1) 	cout << "	   L    Interfaces between FPGA and HPS are not all global disabled"<<endl;
-		else					cout << "	   L    [INTERFACE GLOBAL RESET] All interfaces between FPGA and HPS are disabled."<<endl;
-
+		if (global_inf_en==1) 	cout << "	   L    [Y] Interfaces between FPGA and HPS are not all global disabled"<<endl;
+		else					cout << "	   L    [N] [INTERFACE GLOBAL RESET] All interfaces between FPGA and HPS are disabled."<<endl;
+		
+		cout << "	        General Signals of the HPS Module"<<endl;
 		cout << indiv2str(indiv_code)<<endl;
+		cout << "	        Specific module signals Enabled/Disabled"<<endl;
+		cout << moudleEn2str(signal_en)<<endl;
+
+		cout << "#  Clock Manager Settings" <<endl;
+		cout << clockCtrl2str(clock_ctrl)<< endl;
 	}
 
 	deinit();
